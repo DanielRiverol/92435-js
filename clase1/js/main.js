3471d46b45807b807f38d0843cb234f5b43df78d
Daniel Riverol <dannyriverol@hotmail.com>
2026-07-23 20:56:32 -0300

    Clase 1

---
+++ clase1/js/main.js
@@ -0,0 +1,63 @@
+// Variables
+let nombre; //declaracion
+nombre = "Juan"; //asignacion de un valor
+let apellido = "Lopez"; //inicializacion
+console.log(nombre);
+console.log(apellido);
+apellido = "Rivas"; // reasignacion de valor
+console.log(apellido);
+console.log(typeof apellido);
+apellido = true;
+console.log(typeof apellido);
+let edad = 43;
+console.log(edad);
+// EJEMPLO JAVA: int edad = "43"
+// let temp = 16.4; //decimal
+// console.log(temp);
+
+// let activo = true;
+// console.log(activo);
+// activo=false
+// console.log(activo);
+// console.log(typeof activo);
+// let patente = "AI234UO";
+// console.log(patente);
+
+// const PI = 3.14156; //siempre se inicializan
+// const GRAVEDAD = 9.8;
+// console.log(GRAVEDAD);
+// console.log(typeof GRAVEDAD);
+
+// //  PI=4 //no se pude hacer
+// console.log(PI);
+// console.log(typeof PI);
+
+//  typeof
+
+let variable;
+
+// métodos de salida
+console.log("Salida por consola");
+// alert("Metodo de salida super molesto")
+// medtodo de entrada prompt
+let numero1 = parseFloat(prompt("Ingresa el primer nuemro"));
+let numero2 = prompt("Ingresa el segundo nuemro");
+numero2 = parseFloat(numero2)
+// parseo parseInt() parseFloat() Number()
+
+
+// los prompt siempre deviuelve string
+console.log(typeof numero1);
+
+console.log(typeof numero2);
+
+console.log(numero1 + numero2);//contatenacion
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+
+
+console.log("Hola"+ 34);
+console.log(3 + 5);
+
+
